5f7ebec0efc3330c7ba0b6276a7c7fe3462490b8
ryanctruong <ryanctruong03@gmail.com>
2023-09-19 11:59:52 -0500

    changed context actions

--- app/src/main/java/com/example/repairorder/MainActivity.java
+++ app/src/main/java/com/example/repairorder/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.repairorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +14,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     View.OnClickListener submitListener = new View.OnClickListener() {
+        @SuppressLint({"SetTextI18n", "DefaultLocale"})
         @Override
         public void onClick(View view) {
             order = orderType.getText().toString();
@@ -21,19 +25,20 @@ public class MainActivity extends AppCompatActivity {
             cost_labor = Double.parseDouble(labor.getText().toString());
 
 
-            double sub = cost_inspection + cost_paint + cost_part + cost_labor;
-            subTotalText.setText("$" + String.valueOf(String.format("%.2f",sub)));
-            taxText.setText("$" + String.valueOf(String.format("%.2f",sub/9.25)));
-            totalText.setText("$" + String.valueOf(String.format("%.2f",sub + (sub/9.25))));
+            double t = cost_inspection + cost_paint + cost_part + cost_labor;
+            subTotalText.setText("$" + String.format("%.2f",t));
+            taxText.setText("$" + String.format("%.2f",t/9.25));
+            totalText.setText("$" + String.format("%.2f", t + (t / 9.25)));
 
         }
     };
     Button submitButton;
     EditText orderType,technicianET, inspection, paint, parts, labor;
     TextView subTotalText, taxText, totalText;
-    String order, tech, ins,pain,part,lab;
+    String order, tech;
 
     double cost_inspection, cost_paint, cost_part, cost_labor;
+    //double sub = 0;
 
 
     @Override
@@ -54,6 +59,95 @@ public class MainActivity extends AppCompatActivity {
         taxText = findViewById(R.id.tax_value);
         totalText = findViewById(R.id.total_value);
 
+        inspection.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                cost_inspection = Double.parseDouble(inspection.getText().toString());
+
+                double sub = cost_inspection;
+                subTotalText.setText("$" + String.format("%.2f", sub));
+                taxText.setText("$" + String.format("%.2f", sub / 9.25));
+                totalText.setText("$" + String.format("%.2f", sub + (sub / 9.25)));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        paint.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                cost_paint = Double.parseDouble(paint.getText().toString());
+
+                double sub = cost_inspection + cost_paint;
+                subTotalText.setText("$" + String.format("%.2f", sub));
+                taxText.setText("$" + String.format("%.2f", sub / 9.25));
+                totalText.setText("$" + String.format("%.2f", sub + (sub / 9.25)));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        parts.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                cost_part = Double.parseDouble(parts.getText().toString());
+
+                double sub = cost_inspection + cost_paint + cost_part;
+                subTotalText.setText("$" + String.format("%.2f", sub));
+                taxText.setText("$" + String.format("%.2f", sub / 9.25));
+                totalText.setText("$" + String.format("%.2f", sub + (sub / 9.25)));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+        labor.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                cost_labor = Double.parseDouble(labor.getText().toString());
+
+                double sub = cost_inspection + cost_paint + cost_part + cost_labor;
+                subTotalText.setText("$" + String.format("%.2f", sub));
+                taxText.setText("$" + String.format("%.2f", sub / 9.25));
+                totalText.setText("$" + String.format("%.2f", sub + (sub / 9.25)));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
         submitButton.setOnClickListener(submitListener);
     }
+
+
 }
